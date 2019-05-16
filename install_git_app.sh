e01766f0339a4db145687cad884fa3b1a7ed7494
mahmoodsheikh36 <mahmod.m2015@gmail.cm>
2019-05-16 11:53:57 +0300

    no need for that anymore

--- install_git_app.sh
+++
@@ -1,5 +0,0 @@
-# install vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
