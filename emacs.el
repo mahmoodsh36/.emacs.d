1e6ce7b23151058229a41bf597427c5984eb1480
mahmoodsheikh36 <mahmod.m2015@gmail.cm>
2019-05-17 07:58:06 +0300

    updated emacs config

--- emacs.el
+++ emacs.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "946e871c780b159c4bb9f580537e5d2f7dba1411143194447604ecbaf01bd90c" "88049c35e4a6cedd4437ff6b093230b687d8a1fb65408ef17bfcf9b7338734f6" "c616e584f7268aa3b63d08045a912b50863a34e7ea83e35fcab8537b75741956" "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" default)))
+    ("c616e584f7268aa3b63d08045a912b50863a34e7ea83e35fcab8537b75741956" "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" "")))
  '(package-selected-packages
    (quote
     (helm linum-relative idle-highlight-mode airline-themes powerline expand-region projectile magit darktooth-theme monokai-theme use-package)))
