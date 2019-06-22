0d7f0fb114be8d2d357db953ce4f75dad3a278bb
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2019-06-23 00:51:24 +0300

    zenburn theme, disable cursor blink, underscore as part of word

--- emacs.el
+++ emacs.el
@@ -6,14 +6,12 @@
  '(custom-safe-themes
    (quote
     ("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" default)))
- '(global-company-mode t)
  '(package-selected-packages
    (quote
-    (gruvbox-theme zenburn-theme company evil-numbers fzf evil-surround eyebrowse spacemacs-theme dracula-theme ivy evil-magit treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs doom-modeline doom-line helm htlm linum-relative use-package)))
+    (rainbow-delimiters rainbow-delimeters zenburn-theme company evil-numbers fzf evil-surround eyebrowse spacemacs-theme dracula-theme ivy evil-magit treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs doom-modeline doom-line helm htlm linum-relative use-package)))
  '(spacemacs-theme-comment-bg nil)
  '(spacemacs-theme-comment-italic 1)
- '(spacemacs-theme-keyword-italic t)
- '(which-function-mode t))
+ '(spacemacs-theme-keyword-italic t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -55,6 +53,16 @@
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup")))
 ;; kill current buffer without prompt
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
+;; disable new line at the end
+;; dunno if the 1st line is necessary
+;; (setq mode-require-final-newline nil)
+;; (setq require-final-newline nil)
+;; disable cursor blink
+(blink-cursor-mode 0)
+;; treat underscore as part of word
+(defun underscore-part-of-word-hook ()
+  (modify-syntax-entry ?_ "w"))
+(add-hook 'prog-mode-hook 'underscore-part-of-word-hook)
 
 ;; evil-mode
 (use-package evil
@@ -83,10 +91,10 @@
 ;;   :ensure t
 ;;   :defer t
 ;;   :init (load-theme 'spacemacs-dark t))
-(use-package gruvbox-theme
+(use-package zenburn-theme
   :ensure t
   :config
-  (load-theme 'gruvbox t))
+  (load-theme 'zenburn t))
 
 ;; helm
 (use-package helm
@@ -181,6 +189,25 @@
   (add-hook 'after-init-hook 'global-company-mode)
   (global-set-key (kbd "C-c c") 'company-complete))
 
-;; clear the eshell buffer.
-(defun eshell/clear ()      
-   (let ((eshell-buffer-maximum-lines 0)) (eshell-truncate-buffer)))
+(use-package rainbow-delimiters
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
+;; clear command to clear the eshell buffer.
+(defun eshell/clear ()
+  (let ((eshell-buffer-maximum-lines 0)) (eshell-truncate-buffer)))
+(defun eshell-define-clear-command ()
+  (interactive)
+  (defun eshell/clear ()
+    (let ((eshell-buffer-maximum-lines 0)) (eshell-truncate-buffer))))
+(add-hook 'eshell-load-hook #'eshell-define-clear-command)
+
+
+;; function to refactor json files
+(defun beautify-json ()
+  (interactive)
+  (let ((b (if mark-active (min (point) (mark)) (point-min)))
+        (e (if mark-active (max (point) (mark)) (point-max))))
+    (shell-command-on-region b e
+     "python -mjson.tool" (current-buffer) t)))
