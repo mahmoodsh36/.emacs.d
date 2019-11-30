085394f5c3493b0e8a57f68519fc1fca87b6f64c
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2019-11-30 23:28:13 +0200

    update

--- .config/mpv/scripts/keys.lua
+++ .config/mpv/scripts/keys.lua
@@ -1,5 +1,5 @@
 function video_downloader()
     vidurl = mp.get_property("path")
-    os.execute('notify-send "downloading video" && youtube-dl -o "/home/$USER/media/vid/%(title)s-%(id)s.%(ext)s" ' .. vidurl .. ' && notify-send "finished downloading video"')
+    os.execute('notify-send "downloading video" && youtube-dl -o "/home/$USER/media/vid/girls/%(title)s-%(id)s.%(ext)s" "' .. vidurl .. '" 2>&1 > /home/$USER/.cache/mpv_dl_log && notify-send "finished downloading video" &')
 end
 mp.add_key_binding("d", "download_video", video_downloader)
