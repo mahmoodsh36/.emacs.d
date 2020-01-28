c8600788627867e50e2cd9639c24e387a9e8dbba
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-01-28 15:04:38 +0200

    configured qutebrowser fonts, shortcuts, etc

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,7 +2,7 @@
 
 c.url.searchengines = {
         "DEFAULT": "https://duckduckgo.com/?q={}",
-        "y": "https://www.youtube.com/results?search_query={}",
+        "y": "https://www.youtube.com/results?hl=en&search_query={}",
         "g": "https://www.google.com/search?q={}",
         'i': 'https://duckduckgo.com/?q={}&iar=images&iax=images&ia=images',
         'd': 'https://duckduckgo.com/?q={}',
@@ -15,12 +15,37 @@ c.aliases = {
         "gg": "open https://google.com",
         "mu": "open https://mahmoodsheikh.com/music",
         "gt": "open https://mahmoodsheikh.com/github",
+        "r": "open https://reddit.com",
+        "test": "spawn --userscript test.sh",
 }
 
-config.bind('<Ctrl-Shift-y>', 'hint links spawn --detach mpv --force-window yes {hint-url}')
+config.bind('<Ctrl-Shift-v>', 'hint links spawn --detach mpv --keep-open --force-window yes {hint-url}')
+config.bind('<Ctrl-Shift-y>', 'hint links spawn --detach sh -c \'url={hint-url}; notify-send "$url"; echo "$url" | xclip -selection clipboard\'')
 
 c.content.user_stylesheets = [
-        "style.css"
+        #"style.css"
 ]
 
 c.colors.webpage.bg = 'black'
+
+c.content.cookies.accept = 'never'
+c.content.cookies.store = False
+
+c.auto_save.session = True
+
+font_size = '15pt'
+c.fonts.completion.category = font_size
+c.fonts.completion.entry = font_size
+c.fonts.contextmenu = font_size
+c.fonts.debug_console = font_size
+c.fonts.downloads = font_size
+c.fonts.hints = font_size
+c.fonts.keyhint = font_size
+c.fonts.messages.error = font_size
+c.fonts.messages.info = font_size
+c.fonts.messages.warning = font_size
+c.fonts.prompts = font_size
+c.fonts.statusbar = font_size
+c.fonts.tabs = font_size
+c.fonts.web.size.minimum = 15
+c.fonts.web.size.default = 15
