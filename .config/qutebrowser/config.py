f47c7c49cd09435a01be62a3f999a90e4483e561
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-05-23 22:59:53 +0300

    remove a few from host blocking list

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -67,16 +67,16 @@ config.source("jblock/jblock/integrations/qutebrowser.py")
 config.set(
     "content.host_blocking.lists",
     [
-        "https://easylist.to/easylist/easylist.txt",
-        "https://easylist.to/easylist/easyprivacy.txt",
-        "https://easylist.to/easylist/fanboy-annoyance.txt",
+        #"https://easylist.to/easylist/easylist.txt",
+        #"https://easylist.to/easylist/easyprivacy.txt",
+        #"https://easylist.to/easylist/fanboy-annoyance.txt",
         "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt",
         "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances.txt",
         "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt",
         "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt",
         "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resource-abuse.txt",
         "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/unbreak.txt",
-        "https://www.malwaredomainlist.com/hostslist/hosts.txt",
-        "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=1&mimetype=plaintext",
+        #"https://www.malwaredomainlist.com/hostslist/hosts.txt",
+        #"https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=1&mimetype=plaintext",
     ],
 )
