cf20418744c17a8a8089f1ba80778a9bdea7acd6
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2021-12-23 17:55:58 +0200

    add more keybindings

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -16,24 +16,27 @@ c.aliases = {
         "yt": "open https://youtube.com",
         "dk": "open https://start.duckduckgo.com",
         "gg": "open https://google.com",
-        "mu": "open https://mahmoodsheikh.com/music",
-        "gt": "open https://mahmoodsheikh.com/github",
+        "gt": "open https://github.com/mahmoodsheikh36/",
         "pb": "open https://thepiratebay.org",
         "ru": "open https://rutracker.org",
         "r": "open https://reddit.com",
         "test": "spawn --userscript test.sh",
         "mu": "open https://4chan.org/mu",
         "t": "open trackifyapp.net/spotify/top_users",
+        "me": "open mahmoodsheikh.com",
+        "gm": "open gmail.com",
         "q": "quit",
 }
 
-config.bind('<Ctrl-Shift-v>', 'hint links spawn --detach mpv --keep-open --force-window yes {hint-url}')
+config.bind('<Ctrl-Shift-v>', 'hint links spawn --detach open_yt_vid.sh {hint-url}')
+config.bind('<Ctrl-Shift-f>', 'hint links spawn --detach mpv --keep-open --force-window=immediate {hint-url}')
 config.bind('<Ctrl-Shift-y>', 'hint links spawn --detach sh -c \'url={hint-url}; notify-send "$url"; echo "$url" | tr -d "\\n" | xclip -selection clipboard\'')
 config.bind('<Ctrl-Shift-m>', 'hint links spawn --detach add_magnet.sh "{hint-url}"')
 
 config.bind('Iv', 'hint images spawn sh -c "open_url_image.sh {hint-url}"')
 config.bind('Is', 'hint images run open -t https://www.google.com/searchbyimage?&image_url={hint-url}')
 config.bind('Idw', 'hint images spawn terminal_with_cmd.sh "curl -O {hint-url} --output-dir ~/data/images/wal/ && notify-send downloaded-image"')
+config.bind('Ia', 'hint images spawn sh -c "touch ~/workspace/tmp/img && echo \"{hint-url}\" >> ~/workspace/tmp/img && notify-send \"added image\""')
 
 c.colors.webpage.bg = 'black'
 c.colors.webpage.darkmode.enabled = True
