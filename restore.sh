d46b307489a18e1e12eb3700317fb2450c254228
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-01-03 21:07:10 +0200

    restore some more files

--- restore.sh
+++ restore.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env sh
 
 mkdir ~/.emacs.d/
-ln -s "$HOME/workspace/dotfiles/.emacs.d/*" "$HOME/.emacs.d/"
+ln -sf "$HOME/work/dotfiles/.emacs.d/"* "$HOME/.emacs.d/"
+
+ln -sf "$HOME/work/dotfiles/.gitconfig" "$HOME/"
 
 for filename in .xinitrc .zshrc .zprofile .Xresources .vimrc .tmux.conf; do
-	ln -s "$HOME/workspace/dotfiles/$filename" "$HOME/"
+	ln -sf "$HOME/work/dotfiles/$filename" "$HOME/"
 done
 
 mkdir "$HOME/.config/"
 for filename in alacritty compton.conf gtk-3.0 mimeapps.list mpv vifm qutebrowser kitty\
 	rofi sxhkd sxiv user-dirs.dirs transmission-daemon zathura; do
-	ln -s "$HOME/workspace/dotfiles/.config/$filename" "$HOME/.config/"
+	ln -sf "$HOME/work/dotfiles/.config/$filename" "$HOME/.config/"
 done
 
-ln -s "$HOME/workspace/dotfiles/.config/transmission-daemon" "$HOME/.config/transmission"
+ln -sf "$HOME/work/dotfiles/.config/transmission-daemon" "$HOME/.config/transmission"
 
 mkdir "$HOME/.config/guix/"
-ln -s "$HOME/workspace/dotfiles/channels.scm" "$HOME/.config/guix/"
-sudo ln -sf "$HOME/workspace/dotfiles/config.scm" "/etc/config.scm"
+ln -sf "$HOME/work/dotfiles/channels.scm" "$HOME/.config/guix/"
+sudo ln -sf "$HOME/work/dotfiles/config.scm" "/etc/config.scm"
 
 mkdir "$HOME/.config/nvim/"
-ln -s "$HOME/workspace/dotfiles/.vimrc" "$HOME/.config/nvim/init.vim"
+ln -sf "$HOME/work/dotfiles/.vimrc" "$HOME/.config/nvim/init.vim"
