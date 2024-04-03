7e38458c745c8d371665c0302daf045cc757cad1
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-04-03 14:04:54 +0300

    move some android stuff to their own file

---
+++ lisp/setup-android.el
@@ -0,0 +1,24 @@
+;; stuff that is specifically related to emacs on android
+
+(defun is-android-system ()
+  (or (string-equal system-type "android")
+      (eq (let ((inhibit-message t)) (shell-command "command -v termux-setup-storage" nil)) 0)))
+
+;; for termux binaries on android
+(when (is-android-system)
+  (setenv "PATH" (format "%s:%s" "/data/data/com.termux/files/usr/bin"
+                         (getenv "PATH")))
+  (setenv "LD_LIBRARY_PATH" (format "%s:%s"
+                                    "/data/data/com.termux/files/usr/lib"
+                                    (getenv "LD_LIBRARY_PATH")))
+  (push "/data/data/com.termux/files/usr/bin" exec-path))
+
+;; disable native comp on android to prevent exhausting the cpu, doesnt work?
+(when (is-android-system)
+  (setq native-comp-speed -1))
+
+;; some annoying error messages on android's termux keep interfering with shell-command-to-string, redefine it here to discard all error messages
+(when (is-android-system)
+  (defalias 'shell-command-to-string shell-command-to-string-no-stderr))
+
+(provide 'setup-android)
