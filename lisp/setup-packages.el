08a5146e1204ecdd2a2a0d27a68fffe064c1d582
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-04-06 09:43:34 +0300

    restore marginalia

--- lisp/setup-packages.el
+++ lisp/setup-packages.el
@@ -127,13 +127,13 @@
   (add-to-list 'completion-at-point-functions #'cape-tex)
   ;; (add-to-list 'completion-at-point-functions #'cape-line) ;; too intrusive, dont enable
   (add-to-list 'completion-at-point-functions #'cape-keyword)
-  (add-to-list 'completion-at-point-functions #'cape-elisp-block)
+  ;; (add-to-list 'completion-at-point-functions #'cape-elisp-block)
   ;; (add-to-list 'completion-at-point-functions #'cape-history) ;; too intrusive
   (add-to-list 'completion-at-point-functions #'cape-sgml)
   (add-to-list 'completion-at-point-functions #'cape-rfc1345)
   (add-to-list 'completion-at-point-functions #'cape-abbrev)
   (add-to-list 'completion-at-point-functions #'cape-dict)
-  (add-to-list 'completion-at-point-functions #'cape-elisp-symbol)
+  ;; (add-to-list 'completion-at-point-functions #'cape-elisp-symbol)
   )
 
 ;; for some reason ispell completion is enabled in org mode
@@ -650,11 +650,11 @@
 
 (use-package consult)
 
-;; significantly slows down C-h f and others
-;; (use-package marginalia
-;;   :config
-;;   (marginalia-mode))
+(use-package marginalia
+  :config
+  (marginalia-mode))
 
+;; significantly slows down C-h f and others
 ;; (use-package all-the-icons-completion
 ;;   :after (all-the-icons marginalia)
 ;;   :config
