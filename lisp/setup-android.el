fc3736ffd2cb11449198b002d170b7dea211c880
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-04-12 11:03:56 +0300

    "fix" for call-process-shell-command stderr on emacs termux

--- lisp/setup-android.el
+++ lisp/setup-android.el
@@ -19,6 +19,12 @@
 
 ;; some annoying error messages on android's termux keep interfering with shell-command-to-string, redefine it here to discard all error messages
 (when (is-android-system)
-  (defalias 'shell-command-to-string 'shell-command-to-string-no-stderr))
+  (defalias 'shell-command-to-string 'shell-command-to-string-no-stderr)
+  (defalias 'call-process-shell-command 'my-call-process-shell-command))
+
+(defun my-call-process-shell-command (cmd &optional infile buffer display)
+  (with-current-buffer buffer
+    (insert (shell-command-to-string-no-stderr cmd))
+    0))
 
 (provide 'setup-android)
