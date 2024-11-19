6615c7a1f79e82d4f29455b7d8affd09a711057e
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-11-19 09:48:55 +0200

    minor changes

--- lisp/config-org.el
+++ lisp/config-org.el
@@ -1954,7 +1954,7 @@ KEYWORDS is a list of keyword strings, like '(\"TITLE\" \"AUTHOR\")."
          (org-element-use-cache)
          (org-mode-hook)
          (files (if (atom ,files) (list ,files) ,files))
-         (gc-cons-threshold 1000000000) ;; 1gb
+         (gc-cons-threshold 100000000) ;; 100mb
          (coding-system-for-read 'utf-8))
      (with-temp-buffer
        (buffer-disable-undo)
