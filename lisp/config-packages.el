25b28209b3a511969fd9099b701e67e1d989b826
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-05-05 16:42:20 +0300

    add lua-mode

--- lisp/config-packages.el
+++ lisp/config-packages.el
@@ -1470,4 +1470,16 @@ Cancel the previous one if present."
                                       gptel-tools))))
             tools)))
 
+(use-package lua-mode
+  :init
+  (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+  (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+  (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+  :config
+  (when (executable-find "lua-language-server")
+    (require 'eglot)
+    (add-to-list 'eglot-server-programs
+                 `(lua-mode . ("lua-language-server")))
+    (add-hook 'lua-mode-hook 'eglot-ensure)))
+
 (provide 'config-packages)
